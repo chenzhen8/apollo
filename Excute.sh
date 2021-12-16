a3d5349c7b731f6cb3afe4f522e7c7045f37b35f
chenzhen8 <zjgchenzhen@126.com>
2021-12-16 11:21:00 +0800

    script

---
+++ Excute.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sh /Ted_Script/First_T.sh
+sh /Ted_Script/Second_T.sh
+sh /Ted_Script/Third_T.sh
+
+
--- Ted_Script/Third_T.sh
+++ Ted_Script/Third_T.sh
@@ -3,8 +3,8 @@ cd /home/$USER
 mkdir T
 cd T
 git clone https://github.com/ApolloAuto/apollo.git
-unzip apollo-master.zip -d /home/$USER/T
-cd /home/$USER/T/apollo-master/
+#unzip apollo-master.zip -d /home/$USER/T
+cd /home/$USER/T/apollo/
 bash docker/scripts/dev_start.sh
 bash docker/scripts/dev_into.sh
 
